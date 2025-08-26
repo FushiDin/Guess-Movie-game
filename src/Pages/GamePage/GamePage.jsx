c26e5a00c927c601dda37596319a7b1f581249e4
FushiDin <sairdintujgunov@gmail.com>
2025-08-26 13:29:19 +0500

    Style put

--- src/Pages/GamePage/GamePage.jsx
+++ src/Pages/GamePage/GamePage.jsx
@@ -1,9 +1,25 @@
+import { Link } from 'react-router-dom';
 import styles from './GamePage.module.css'
 
 function GamePage() {
     return (
-        <div>
+        <div className={styles.gamePage}>
+            <div className={styles.header}>
+                <Link to="/">Выйти</Link>
+                <p>?</p>
+                <Link>Добавить фильм</Link>  
+            </div>
 
+            <div className={styles.left}>
+                <img src="assets/OnePiceCard.jpg" alt="OnePice" />
+                <h4>One Pice</h4>
+            </div>
+
+            <div className={styles.right}>
+                <p>Это аниме про парня в соломенной шляпе, который собирает команду друзей <br/> и путешествует по морям, чтобы найти величайшее сокровище мира.</p>
+                <input type="text" />
+                <button>Ответить</button>
+            </div>
         </div>
     )
 }
